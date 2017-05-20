f2d5193329d9f6ed79b495523ab3b3f2f801d04b
Stefan Birrer <stefan.birrer@phenixp2p.com>
2017-05-19 19:14:35 -0500

    Room API Release (#WSDK-93) Fixed
    Change-Id: I74fe36950a363a505b0f44ee6545bc12aca746cc
    Reviewed-on: http://gerrit.phenixp2p.com/2872
    Reviewed-by: Stefan Birrer <stefan.birrer@phenixp2p.com>
    Tested-by: SDK Web Bundle
    Tested-by: SDK Web Coverage
    Tested-by: SDK Web
    Reviewed-by: Dylan Yurgionas <dylan.yurgionas@phenixp2p.com>

--- dist/phenix-web-sdk.js
+++ dist/phenix-web-sdk.js
@@ -874,8 +874,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	    var networkDisconnectHysteresisInterval = 0;
 
 	    function AnalytixAppender() {
-	        this._environmentName = 'development' || '?';
-	        this._environmentVersion = '2017-05-19T22:45:31Z' || '?';
+	        this._environmentName = 'production' || '?';
+	        this._environmentVersion = '2017-05-20T00:13:08Z' || '?';
 	        this._loggingUrl = '/analytix/logs';
 	        this._source = (rtc.browser || 'Browser') + '/' + (rtc.browserVersion || '?');
 	        this._protocol = new MQProtocol();
@@ -1069,7 +1069,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    'use strict';
 
 	    function Http() {
-	        this._version = '2017-05-19T22:45:31Z';
+	        this._version = '2017-05-20T00:13:08Z';
 	    }
 
 	    Http.prototype.getWithRetry = function getWithRetry(url, callback, maxAttempts, attempt) {
@@ -4298,7 +4298,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            }
 	        ]
 	    });
-	    var sdkVersion = '2017-05-19T22:45:31Z';
+	    var sdkVersion = '2017-05-20T00:13:08Z';
 	    var defaultChromePCastScreenSharingExtensionId = 'icngjadgidcmifnehjcielbmiapkhjpn';
 	    var defaultFirefoxPCastScreenSharingAddOn = _.freeze({
 	        url: 'https://addons.mozilla.org/firefox/downloads/file/474686/pcast_screen_sharing-1.0.3-an+fx.xpi',
