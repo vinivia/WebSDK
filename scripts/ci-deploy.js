cfb1beaa0ef5b67a46dc4e65b4af61cf382e3399
Dylan Yurgionas <dylan.yurgionas@phenixrts.com>
2018-07-17 17:25:52 -0500

    Add ci-deploy script (#WSDK-301) Fixed
    Change-Id: If7e4060670f4c1142a853d67d32f413155002378
    Reviewed-on: http://gerrit.phenixrts.com/5273
    Reviewed-by: Dylan Yurgionas <dylan.yurgionas@phenixrts.com>
    Tested-by: Dylan Yurgionas <dylan.yurgionas@phenixrts.com>
    Tested-by: SDK Web Coverage
    Tested-by: SDK Web Bundle
    Tested-by: SDK Web
    Reviewed-by: Stefan Birrer <stefan.birrer@phenixrts.com>

---
+++ scripts/ci-deploy.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2018 PhenixP2P Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const runner = require('./runner');
+const packageJson = require('../package.json');
+const version = packageJson.version;
+
+// Tag must be manually pushed via `git push --tags`
+runner.runCommands([
+    'node --version',
+    'npm --version',
+    'npm publish',
+    'git tag -a v' + version + ' -m "Release ' + version + '"'
+]);
---
+++ scripts/version.js
@@ -0,0 +1,6 @@
+/**
+ * Copyright 2018 PhenixP2P Inc. Confidential and Proprietary. All Rights Reserved.
+ */
+const packageJson = require('../package.json');
+
+process.stdout.write(packageJson.version);
